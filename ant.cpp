99924a16085d147fb2877e784ccde7ec90404f7a
pablomoredavegace <pablomoredace@gmail.com>
2026-02-25 13:06:59 +0100

    feat(ant): update ant behavior with most used cases

--- ant.cpp
+++ ant.cpp
@@ -2,7 +2,9 @@
 #include "ant.h"
 #include <iostream>
 
-Ant::Ant(int x, int y, Direction dir) : PosX(x), PosY(y), Dir(dir) {}
+Ant::Ant(int x, int y, Direction dir, std::string type, std::string antColor) : PosX(x), PosY(y), Dir(dir), 
+TypeAnt(std::move(type)), ColorAnt(std::move(antColor)) {}
+
 
 void Ant::TurnLeft() {
   switch (Dir) {
@@ -64,6 +66,53 @@ void Ant::Move() {
   }
 }
 
+void Ant::BaseStep(Tape& tape, std::string_view rule) {
+  if(!tape.Limits(PosX, PosY)) {
+    return;
+  }
+
+  const std::uint16_t c = tape.GetColor(PosX, PosY);
+
+  const std::uint16_t c2 = NextColor(tape, c);
+  tape.CambioColor(PosX, PosY, c2);
+
+  if(!rule.empty()) {
+    const char turn = rule[static_cast<std::size_t>(c2) % rule.size()];
+    if(turn == 'D' || turn == 'd') {
+      TurnRight();
+    }
+    else TurnLeft();
+  }
+   
+  Move();
+
+}
+
+AntDI::AntDI(int x, int y, Direction dir, std::string ColorAnt) : Ant(x, y, dir, "DI", std::move(ColorAnt)) {}
+
+void AntDI::Step(Tape& tape) {
+  BaseStep(tape, "DI");
+}
+
+AntDDII::AntDDII(int x, int y, Direction dir, std::string ColorAnt) : Ant(x, y, dir, "DDII", std::move(ColorAnt)) {}
+
+void AntDDII::Step(Tape& tape) {
+  BaseStep(tape, "DDII");
+}
+
+AntIIDD::AntIIDD(int x, int y, Direction dir, std::string ColorAnt) : Ant(x, y, dir, "IIDD", std::move(ColorAnt)) {}
+
+void AntIIDD::Step(Tape& tape) {
+  BaseStep(tape, "IIDD");
+}
+
+AntDIDI::AntDIDI(int x, int y, Direction dir, std::string ColorAnt) : Ant(x, y, dir, "DIDI", std::move(ColorAnt)) {}
+
+void AntDIDI::Step(Tape& tape) {
+  BaseStep(tape, "DIDI");
+}
+
+/*
 void Ant::Step(Tape& tape) {
   const bool black = tape.IsBlack(PosX, PosY);
   tape.CambioColor(PosX, PosY);
@@ -72,7 +121,7 @@ void Ant::Step(Tape& tape) {
   } else { TurnLeft(); }
   Move();
 }
-
+*/
 char Ant::Orientation() const {
   switch (Dir) {
     case Direction::Up: 
@@ -93,6 +142,8 @@ char Ant::Orientation() const {
 }
 
 std::ostream& operator<<(std::ostream& os, const Ant& ant) {
-  os << "Ant(x=" << ant.GetX() << ", y=" << ant.GetY() << ", dir=" << ant.Orientation() << ")";
+  if(!ant.GetAntColor().empty()) os << ant.GetAntColor();
+  os << ant.GetType() << " (" << ant.GetX() << "," << ant.GetY() << ") " << ant.Orientation();
+  if(!ant.GetAntColor().empty()) os << "\033[0m";
   return os;
 }
