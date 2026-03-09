12a5a34ae82c60d5ad7d4ed6181584d096bf44d0
pablomoredavegace <pablomoredace@gmail.com>
2026-03-09 01:54:29 +0100

    feat(tape_sliding): debug sliding tape (core dumped error)

--- tape_sliding.cpp
+++ tape_sliding.cpp
@@ -19,11 +19,11 @@ std::size_t TapeSliding::GetSizeY() const noexcept { return static_cast<std::siz
 
 void TapeSliding::PermX(int x) {
   while (x < x_min_) {
-    for (int y = y_min_; y <= y_max_; ++y) grid_[y].PushFront(0);
+    for (int yy = y_min_; yy <= y_max_; ++yy) grid_[yy].PushFront(0);
     --x_min_;
   }
   while (x > x_max_) {
-    for (int y = y_min_; y <= y_max_; ++y) grid_[y].PushBack(0);
+    for (int yy = y_min_; yy <= y_max_; ++yy) grid_[yy].PushBack(0);
     ++x_max_;
   }
 }
@@ -46,19 +46,15 @@ void TapeSliding::Normal(int& x, int& y, Direction&) {
 
 std::uint16_t TapeSliding::GetColor(int x, int y) const {
   if (x < x_min_ || x > x_max_ || y < y_min_ || y > y_max_) return 0;
-  const std::size_t ix = static_cast<std::size_t>(x - x_min_);
-  const std::size_t iy = static_cast<std::size_t>(y - y_min_);
-  return grid_[iy][ix];
+  return grid_[y][x];
 }
 
 void TapeSliding::CambioColor(int x, int y, std::uint16_t c) {
-  PermX(x);
-  PermY(y);
+  Direction d = Direction::Up;
+  Normal(x, y, d);
   if (GetNumColors() == 0) return;
 
-  const std::size_t ix = static_cast<std::size_t>(x - x_min_);
-  const std::size_t iy = static_cast<std::size_t>(y - y_min_);
-  grid_[iy][ix] = static_cast<std::uint16_t>(c % GetNumColors());
+  grid_[y][x] = static_cast<std::uint16_t>(c % GetNumColors());
 }
 
 std::vector<std::tuple<int,int,std::uint16_t>> TapeSliding::NoWhiteCells() const {
